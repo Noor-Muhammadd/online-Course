260f68bfdbf9d2a15914eb72f9b9ec8ab77eba6e
root <root@localhost>
2020-08-29 06:18:10 +0600

    publishing awesome pages 1598660290201

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import CourseList from "./components/CourseList/CourseList";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+	return (
+		<div className="App">
+			<Header></Header>
+			<CourseList></CourseList>
+			<Footer></Footer>
+		</div>
+	);
+}
+
+export default App;
+https://github.com/nomanme?tab=repositories
